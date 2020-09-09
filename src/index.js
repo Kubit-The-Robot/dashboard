3b85c8c7b4b5026954434e8e0796ef08aeda45fc
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-09 02:22:47 -0300

    🎉 First RENDER!!!!!!!!!!!!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
+import OverReact from './core/overreact';
 
-console.log('hello beautiful!');
+const App = (
+  <div style="background: red">
+    <h1>Hello Kubit</h1>
+    <h2 style="text-align:right">by: Fiap</h2>
+  </div>
+);
+
+OverReact.render(App,
+  document.getElementById('game-canvas')
+);
