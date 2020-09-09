ef01e80de39b3ce4d56a6f5a76931671004fb729
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-08 22:22:24 -0300

    ✨ Add keymirror module

---
+++ src/modules/keymirror.js
@@ -0,0 +1,22 @@
+/**
+ * @desc Creates an object with an mirrored key / values
+ * @param {object} obj
+ * @returns {object} {}
+ */
+
+export const keyMirror = (obj) => {
+  const mirrored = {};
+  let key;
+
+  if (!(obj instanceof Object && !Array.isArray(obj))) {
+    throw new Error('keyMirror(...): Argument must be an object.');
+  }
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      mirrored[key] = key;
+    }
+  }
+
+  return mirrored;
+};
