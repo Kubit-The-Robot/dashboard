bdc8bb1568bdd88b008217811430f8e1b491d4f4
Enzo <enzoabatrov@hotmail.com>
2020-09-08 22:52:16 -0300

    feat(fakeredux): started redux coding

--- src/core/redux/index.js
+++ src/core/redux/index.js
@@ -1,4 +1,18 @@
+import {
+    Store
+} from "./store";
 
 class Redux {
+    store = null
     
+    constructor(store = new Store()) {
+        this.store = store;
+        if (!store) {
+            this.store = new Store();
+        }
+    }
 }
+
+export {
+    Redux
+};
--- src/core/redux/store.js
+++ src/core/redux/store.js
@@ -1,3 +1,31 @@
+import { v4 } from './utils/uuid';
+
+import {SETUP_STATE} from './constants';
+
+/**
+ * Reducer Description
+ * @name Reducer
+ * @function
+ * @param {Object} state The initial state or the state that is being updated
+ * @param {string} action The action that will be taken when the function is called
+ */
+
 class Store {
-    
+    state = {};
+    /**
+     * @param {Reducer} reducers Defines the Reducers that will be stored into the application
+     */
+    constructor(reducers = {}) {
+        for (const reducer in reducers) {
+            if (!(reducers[reducer] instanceof Function)) {
+                return new Error('All reducers must be Functions');
+            }
+
+            const reducerState = reducers[reducer](null, `${SETUP_STATE}/${v4()}`);
+
+            this.state = {...this.state, ...reducerState}
+        }
+    }
 }
+
+export { Store };
