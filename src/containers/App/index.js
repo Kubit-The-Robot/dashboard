12f7fbdb2a9b012299ccefef90f0919542840a8c
Vinícius Ribeiro <vinicius.vrf@gmail.com>
2020-09-12 00:11:22 -0300

    🔥 Remove old connect logic

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,3 @@
 import App from './App';
-import { connect } from '../../core/overdux/connect';
 
-export default connect(
-  ({ test }) => {
-    console.log(test);
-    return test;
-  },
-  () => {}
-)(App);
+export default App;
